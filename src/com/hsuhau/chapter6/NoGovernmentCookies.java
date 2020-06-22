0741ef1bc0d3394f80c34f8e208732a3aba7f1b7
hsuhau <hsuhau@foxmail.com>
2020-06-22 13:20:45 +0800

    第6章 HTTP

---
+++ src/com/hsuhau/chapter6/NoGovernmentCookies.java
@@ -0,0 +1,19 @@
+package com.hsuhau.chapter6;
+
+import java.net.CookiePolicy;
+import java.net.HttpCookie;
+import java.net.URI;
+
+/**
+ * 示例6-1： 阻塞所有 .gov cookie 但接受其他 cookie 的 cookie 策略
+ *
+ * @author hsuhau
+ * @date 2020/6/22 13:13
+ */
+public class NoGovernmentCookies implements CookiePolicy {
+    @Override
+    public boolean shouldAccept(URI uri, HttpCookie cookie) {
+        return !uri.getAuthority().toLowerCase().endsWith(".gov")
+                && !cookie.getDomain().toLowerCase().endsWith(".gov");
+    }
+}
